47ef2c30dcd22f383a8efe6ed76b55ee51eef0aa
Andrey <andrejsatykov@yandex.ru>
2022-12-17 16:57:41 +0300

    Массив двумерный случайные целые числа

---
+++ Task_00/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив размером m * n, заполненный случайными целыми числами.
+Console.Clear();
+
+void ShowArray(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+        Console.Write($"{array[i, j]}");        
+      }
+      Console.WriteLine();
+   }
+} 
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange) // Метод заполнения массива
+{
+   int[,] array = new int[rows, columns];
+   for (int i = 0; i < rows; i++)
+   {
+      for( int j = 0; j < columns; j++)
+      {
+         array[i, j] = new Random().Next(leftRange, rightRange);
+      }
+   }
+   return array;
+}
+
+int EnterNumber(string message) // Метод ввода данных двумерного массива
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// получить от пользователя m и n:
+
+int m = EnterNumber("Введите число строк массива: ");
+int n = EnterNumber("Введите число столбцов массива: ");
+
+// создаем и заполняем двумерный массив:
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+
+// вывести массив на консоль:
+ShowArray(matrix);
+
+
+
+
+
